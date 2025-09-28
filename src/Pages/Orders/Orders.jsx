bdb2eb1b324decfc817f03a86efddb8d14b084e6
Apostle <melekte96@gmail.com>
2025-09-28 15:23:46 -0500

    path corrected

--- src/Pages/Orders/Orders.jsx
+++ src/Pages/Orders/Orders.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import Layout from "../../components/Layout/Layout";
+import Layout from "../../components/LayOut/LayOut";
 import styles from "./Order.module.css";
 import { DataContext } from "../../components/DataProvider/DataProvider";
 import { db } from "../../Utility/firebase";
